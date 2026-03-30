d17799933c5a5ddf6937122c64a7bf7cd0251b6b
Cindy Royal <5922983+cindyroyal@users.noreply.github.com>
2026-03-30 11:34:35 -0500

    Add files via upload

---
+++ simple_swiftui/simple_swiftui/simple_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_swiftuiApp.swift
+//  simple_swiftui
+//
+//  Created by Royal, Cindy L on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct simple_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
